c7622a778a871eb29942984558a5117d3a97f4bd
lingling <zzxcv741@hotmail.com>
2020-04-25 23:04:21 +0800

    calendar finish

--- homework-calendar/calendar.php
+++ homework-calendar/calendar.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Calender</title>
     <link rel="stylesheet" href="reset.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=Roboto&display=swap&family=Fredoka+One&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC&family=Roboto&display=swap" rel="stylesheet">
 </head>
 
-<body>
+<body onload="startTime()">
     <?php
     $this_day = date("d");
     $this_year = date("Y");
@@ -27,28 +27,33 @@
     }
 
     ?>
-  <div class="wrap">
-       <span class="bg_month"><?= $month?></span>
-       <span class="bg_yeard"><?= $year?></span>
-       <span class="bg_mkmonth"><?= date('F', mktime(0, 0, 0, $month)) ?></span>
-  </div>
+    <div class="wrap">
+        <div class="wrap_text">
+            <span><?= $year ?> , </span>
+            <span><?= date('F', mktime(0, 0, 0, $month)) ?></span>
+        </div>
+        <img src="https://i.imgur.com/IlRhXr5.gif" class="wrap_bg" alt="bg2">
+    </div>
     <div class="container">
+        <div class="time"><?= $this_year . " / " . $this_month . " / " . $this_day ?></div>
+        <div id="clock"></div>
         <?php
+
         $firstday = date("w", strtotime(date("$year-$month-01")));
         $lastday  = date("t", strtotime(date("$year-$month-d")));
 
-        if($month + 1 >= 13){
+        if ($month + 1 >= 13) {
             $mnext = 1;
-            $ynext = $year+1;
-        }else {
+            $ynext = $year + 1;
+        } else {
             $mnext = $month + 1;
             $ynext = $year;
         }
 
-        if($month - 1 <= 0 ){
+        if ($month - 1 <= 0) {
             $mlast = 12;
-            $ylast = $year-1;
-        }else {
+            $ylast = $year - 1;
+        } else {
             $mlast = $month - 1;
             $ylast = $year;
         }
@@ -78,9 +83,9 @@
                         if ($day > $lastday) {
                             echo "<td>";
                         } else {
-                            if($is_this_month && ($day == $this_day)){
-                                echo "<td>" . "<span class='today'>" . $day . "</span>" ;
-                            }else{
+                            if ($is_this_month && ($day == $this_day)) {
+                                echo "<td>" . "<span class='today'>" . $day . "</span>";
+                            } else {
                                 echo "<td>" . $day;
                             }
                         }
@@ -90,51 +95,70 @@
                 echo "</tr>";
             }
             echo "</table>";
-            ?> 
-            
-            <div class="btn">
-            <ul>
-            <li><a href="calendar.php?year=<?= $ylast ?>&month=<?= $mlast ?>">Last month</a></li>
-            <form action="?" method="get">
-            </ul>
-                <div class="selec">
-                <span>西元</span>
-                <select name="year" id="">
-                    <?php
-                    for ($j = 100; $j >= 0; $j--) {
-                        $upY = $year - $j;
-                        echo "<option value='$upY'>" . $upY . "</option>";
-                    }
-                    echo "<option value='$year' selected>" . $year . "</option>";
-                    for ($i = 1; $i <= 100; $i++) {
-                        $nextY = $year + $i;
-                        echo "<option value='$nextY'>" . $nextY . "</option>";
-                    }
-                    ?>
-                </select>
-                <span>年</span>
-                <select name="month" id="">
-                    <?php
-                    for ($j = 1; $j <= $month - 1; $j++) {
-                        echo "<option value='$j'>" . $j . "</option>";
-                    }
-                    echo "<option value='$month' selected>" . $month . "</option>";
-                    for ($i = $month + 1; $i < 13; $i++) {
-                        echo "<option value='$i'>" . $i . "</option>";
-                    }
-                    ?>
-                </select>
-                <span>月</span>
-                <input type="submit" value="查詢">
-            </form>
-            </div>
-            <ul>
-                <li><a href="calendar.php?year=<?= $ynext ?>&month=<?= $mnext ?>">Next month</a></li>
-            </ul>
-        </div>
+            ?>
 
+            <div class="btn">
+                <ul>
+                    <li><a class="lastM" href="calendar.php?year=<?= $ylast ?>&month=<?= $mlast ?>">Last month</a></li>
+                    <form action="?" method="get">
+                </ul>
+                <div class="selec">
+                    <span>西元</span>
+                    <select name="year" id="">
+                        <?php
+                        for ($j = 100; $j >= 0; $j--) {
+                            $upY = $year - $j;
+                            echo "<option value='$upY'>" . $upY . "</option>";
+                        }
+                        echo "<option value='$year' selected>" . $year . "</option>";
+                        for ($i = 1; $i <= 100; $i++) {
+                            $nextY = $year + $i;
+                            echo "<option value='$nextY'>" . $nextY . "</option>";
+                        }
+                        ?>
+                    </select>
+                    <span>年</span>
+                    <select name="month" id="">
+                        <?php
+                        for ($j = 1; $j <= $month - 1; $j++) {
+                            echo "<option value='$j'>" . $j . "</option>";
+                        }
+                        echo "<option value='$month' selected>" . $month . "</option>";
+                        for ($i = $month + 1; $i < 13; $i++) {
+                            echo "<option value='$i'>" . $i . "</option>";
+                        }
+                        ?>
+                    </select>
+                    <span>月</span>
+                    <input type="submit" value="查詢">
+                    </form>
+                </div>
+                <ul>
+                    <li><a class="nextM" href="calendar.php?year=<?= $ynext ?>&month=<?= $mnext ?>">Next month</a></li>
+                </ul>
+                <a class="thisM" href="calendar.php">本月</a>
+            </div>
     </div>
 
+
+    <script>
+        function startTime() {
+            var today = new Date();
+            var hh = today.getHours();
+            var mm = today.getMinutes();
+            var ss = today.getSeconds();
+            mm = checkTime(mm);
+            ss = checkTime(ss);
+            document.getElementById('clock').innerHTML = hh + " : " + mm + " : " + ss;
+            var timeoutId = setTimeout(startTime, 500);
+        }
+        function checkTime(i) {
+            if (i < 10) {
+                i = "0" + i;
+            }
+            return i;
+        }
+    </script>
 
 </body>
 
